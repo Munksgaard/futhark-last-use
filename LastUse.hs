faa53ddeb78149c3c4acf8df284ed2fc314942e5
Philip Munksgaard <philip@munksgaard.me>
2020-06-19 12:26:25 +0200

    Some aliasing that actually works?

--- LastUse.hs
+++ LastUse.hs
@@ -8,7 +8,7 @@ import Control.Monad.IO.Class
 import Data.Foldable
 import Data.Function ((&))
 import qualified Data.Map as Map
-import Data.Map (Map)
+import Data.Map (Map, (!?))
 import Futhark.Analysis.Alias (analyseStms)
 import Futhark.IR.Aliases (CanBeAliased)
 import Futhark.IR.KernelsMem (KernelsMem, MemOp (..), Prog (..), freeIn, typeOf)
@@ -18,17 +18,22 @@ import Futhark.IR.Syntax
 import Futhark.Pass
 import Futhark.Pipeline
 
-lastUse :: ASTLore lore => Stms lore -> Map VName Int
-lastUse stms =
-  zip (toList stms) [0 ..]
-    & reverse
-    & foldr helper Map.empty
+lastUse :: ASTLore lore => Map VName Names -> Stms lore -> Map VName Int
+lastUse aliases stms =
+  let withoutAliasing =
+        zip (toList stms) [0 ..]
+          & reverse
+          & foldr helper Map.empty
+   in Map.foldrWithKey applyAliases withoutAliasing withoutAliasing
   where
     helper :: FreeIn a => (a, Int) -> Map VName Int -> Map VName Int
     helper (stm, i) m =
       freeIn stm
         & namesToList
         & foldr (flip Map.insert i) m
+    applyAliases :: VName -> Int -> Map VName Int -> Map VName Int
+    applyAliases vname0 line_num m0 =
+      foldr (\vname m -> Map.insertWith max vname line_num m) m0 (maybe [] namesToList $ aliases !? vname0)
 
 lastUseFun :: (ASTLore lore, CanBeAliased (Op lore)) => FunDef lore -> FutharkM ()
 lastUseFun
@@ -50,21 +55,28 @@ lastUseFun
             show bodyResult
           ]
 
+    let (stms, (aliases, consumed)) = analyseStms Map.empty bodyStms
+
+    zip (toList stms) [0 ..]
+      & fmap (\(stm, i) -> show i ++ ": " ++ show stm)
+      & unlines
+      & putStrLn
+      & liftIO
+
+    pretty aliases
+      & putStrLn
+      & liftIO
+
     zip (toList bodyStms) [0 ..]
       & fmap (\(stm, i) -> show i ++ ": " ++ pretty stm)
       & unlines
       & putStrLn
       & liftIO
 
-    lastUse bodyStms
+    lastUse aliases bodyStms
       & Map.toList
       & fmap (\(vname, line_num) -> pretty vname ++ ": " ++ show line_num)
       & unlines
-      & putStrLn
-      & liftIO
-
-    analyseStms Map.empty bodyStms
-      & pretty
       & putStrLn
       & liftIO
 
